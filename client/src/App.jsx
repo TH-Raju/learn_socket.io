b29049547e759c73c3703fe08437e625ff107b15
TH-Raju <rajukhan894200@gmail.com>
2024-02-04 15:47:18 +0600

    boilerplate complete

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from 'react';
+import {io} from 'socket.io-client'
+
+const App = () => {
+  const socket = io("http://localhost:3000");
+
+  useEffect(()=> {
+    socket.on("connect", () => {
+      console.log("connected", socket.id);
+    })
+  },[]);
+
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
+
+
